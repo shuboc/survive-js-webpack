dc6125b131693cc57c7f0507656fe06062405ff8
Shubo Chao <apiggy0204@gmail.com>
2016-11-07 23:02:25 +0800

    Analyzing Build Statistics

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,7 @@ var config
 
 switch(process.env.npm_lifecycle_event) {
   case 'build':
+  case 'stats':
     config = merge(
       common,
       {
@@ -70,4 +71,6 @@ switch(process.env.npm_lifecycle_event) {
     )
 }
 
-module.exports = validate(config)
+module.exports = validate(config, {
+  quiet: true
+})
